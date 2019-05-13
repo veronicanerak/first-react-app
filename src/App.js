2cc2d6de98881cb14cceb470e67b1cd2e6316d24
Veronica Nerak <veronicanerak@gmail.com>
2019-05-13 09:18:36 -0400

    Initial work on React first app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+const reasons = [
+  { title: 'Top down data flow' },
+  { title: 'Shadow dom'},
+  { title: 'Declarative, hierarchical, functional' }
+];
+
 function App() {
+  const first_variable = "Hello React";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>First APP with react</h1>
+        <h2>{ first_variable }</h2>
+        {reasons.map( (item) =>  { 
+          return <div className="title">{item.title}</div>;
+         })}
+      </div>
     </div>
   );
 }
