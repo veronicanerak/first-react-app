6aec631d44c00e401afb9fb0ec6dc0b0c005e52f
Veronica Nerak <veronicanerak@gmail.com>
2019-05-16 08:47:21 -0400

    Initial theming for Gallery images.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-
-const reasons = [
-  { title: 'Top down data flow' },
-  { title: 'Shadow dom'},
-  { title: 'Declarative, hierarchical, functional' }
-];
+import photos_list from './data/photos.json';
+const Photo = (props) => {
+  console.log("Props", props);
+  return (
+    <div className="photo_row">
+      <div className="photo_title">{props.photo.title}</div>
+      <div className="photo_image"><img alt={props.photo.title} src={props.photo.url} /></div>
+    </div>
+  );
+}
 
 function App() {
-  const first_variable = "Hello React";
+  const first_variable = "Gallery Example";
+  console.log(photos_list);
   return (
     <div className="App">
-      <div>
         <h1>First APP with react</h1>
         <h2>{ first_variable }</h2>
-        {reasons.map( (item) =>  { 
-          return <div className="title">{item.title}</div>;
-         })}
-      </div>
+        <div className="photos_list">
+          {photos_list.slice(0, 10).map(item =>  { 
+            return <Photo photo={item} key={item.id} />;
+           })}
+        </div>
     </div>
   );
 }
