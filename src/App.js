72b22ffa19f74f714db0d743c4da868f1a1a95bf
Veronica Nerak <veronicanerak@gmail.com>
2019-05-23 10:03:15 -0400

    Iterating Albums and Photos props and states.

--- src/App.js
+++ src/App.js
@@ -18,8 +18,14 @@ const Album = (props) => {
   console.log("Props", props);
   return (
     <div className="album">
-      <div className="album_title">{props.album.title}</div>
-
+      <div className="album_title"><span className="album_number">Album # {props.album.id}</span> - {props.album.title}</div>
+      <div className="photos_album">
+        {props.thePhotos.slice(0, 4).map(photo => {
+          return (
+            <Photo photo={photo} key={photo.id} />
+          );
+        })}
+      </div>
     </div>
   );
 }
@@ -28,15 +34,20 @@ function App() {
 
   const [albums] = useState(albumsList);
   const [photos] = useState(photosList);
-  console.log(albums);
-  console.log(photos);
+  //console.log(albums);
+  //console.log(photos);
   return (
     <div className="App">
-        <h1>First APP with react</h1>
-      {albums.slice(0, 10).map(item =>  {
-        const thePhotos = photos.filter(photo => photo.albumId === item.id);
-        return <Album album={item} thePhotos={thePhotos} key={item.id} />;
-      })}
+      <h1>First APP with React - Gallery</h1>
+
+      <div className="albums">
+        {albums.slice(0, 10).map(album =>  {
+          const thePhotos = photos.filter(photo => photo.albumId === album.id);
+            return (
+              <Album album={album} thePhotos={thePhotos} key={album.id} />
+            );
+        })}
+      </div>
     </div>
   );
 }
